85ae17138b0c42aa7c50ec8de22578b83a68c0f7
ahmad3180 <ahmadbadboy2015@gmail.com>
2016-12-08 22:24:54 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "set",
     "get",
     "broadcast",
- sudo_users={dr_fereshte},--sudo users
+ sudo_users={#24461708},--sudo users
     "invite",
     "all",
     "leave_ban",
